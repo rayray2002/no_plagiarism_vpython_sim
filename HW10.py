f7e8fded2786d0332affb8d3575a5dbe6063287e
rayray2002 <rayray2002.huang@gmail.com>
2021-06-22 00:09:55 +0800

    HW10 correct

--- HW10.py
+++ HW10.py
@@ -1,8 +1,10 @@
 from vpython import *
+import numpy as np
 
 fd = 120  # 120Hz
 # (Your Parameters here)
 T = 1 / fd
+w = 2 * pi * fd
 i = 0
 v = 0
 E = 0
@@ -15,6 +17,9 @@ i_old = i
 E0 = 0
 
 stage = 0
+
+t_i = 0
+t_v = 0
 
 t = 0
 dt = 1.0 / (fd * 5000)  # 5000 simulation points per cycle
@@ -31,13 +36,23 @@ E_t = gcurve(color=color.red, graph=scene2)
 # (Your program here)
 while t <= 20 * T:
     if t < 12 * T:
-        v = 36 * sin(2 * pi * fd * t)
-        i_the = 0.40156 * sin(2 * pi * fd * t - 70 / 180 * pi)  # 70 degree is the phase constant
+        v_new = 36 * sin(2 * pi * fd * t)
+        if v * v_new <= 0 and v - v_new > 0:
+            t_v = t
+
+        v = v_new
+        i_the = 0.40156 * sin(2 * pi * fd * t - 70 / 180 * pi)
         if stage == 0:
             if t >= 9 * T:
-                print("t = 9T Compare i to theory:")
-                print("i =", i, "A, i_theory:", i_the, 'A')
+                print("t = 9T Compare i amplitude to theory:")
+                print("i =", abs(i), "A, i_theory:", abs(i_the), 'A')
                 print("Error:", (i - i_the) / i_the * 100, '%')
+
+                print("t = 9T Compare phi to theory:")
+                phi = (t_v - t_i) * w
+                phi_the = np.arctan(-(w * L - 1 / w / C) / R)
+                print("phi =", phi, "rad, phi_theory:", phi_the, 'rad')
+                print("Error:", (phi - phi_the) / phi_the * 100, '%')
                 stage = 1
     else:
         if stage == 1:
@@ -45,17 +60,20 @@ while t <= 20 * T:
             stage = 2
         elif stage == 2:
             if E <= E0 / 10:
-                print("Decay time:", t - 12 * T, 's')
+                print("Decay time t = ", t, 's (', t - 12 * T, 's from t = 12T)')
                 stage = 3
         v = 0
         i_the = 0
 
     i = (v + L * i_old / dt - Q / C) / (R + dt / C + L / dt)
 
+    if i_old * i <= 0 and i_old - i > 0:
+        t_i = t
+
     Q += i * dt
     i_old = i
     t += dt
-    E = i * i * R + Q * Q / C / 2 + L * i * i / 2
+    E = Q * Q / C / 2 + L * i * i / 2
 
     i_t.plot(pos=(t, i))
     i_the_t.plot(pos=(t, i_the))
